d5f31875fa3e2d698293c6deef37af37524a0cc5
YT2335 <ytlego2335@gmail.com>
2025-08-19 00:36:42 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
